b8391bc552264cc7c0a134d3dedaa1719b5667ec
rlawn0738@gmail.com <rlawn0738@gmail.com>
2021-09-25 02:12:34 +0900

    완성

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,182 +1,73 @@
-﻿#include <bangtal>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <bangtal>
 using namespace bangtal;
 
-//x,y,좌표와 object를 입력받아 조각을 저장한다.
-class Piece {
-public:
-	ObjectPtr object;
-	ScenePtr scene;
-	int x, y;
-	Piece(int x, int y, ObjectPtr object,ScenePtr scene) :x(x), y(y), object(object),scene(scene) {};	
-};
+#include<cstdlib>
+#include<ctime>
 
-void change(Piece *piece1, Piece *piece2) {
-	piece1->object->locate(piece1->scene, piece2->x, piece2->y);
-	piece2->object->locate(piece2->scene, piece1->x, piece1->y);
+int blank;
+ObjectPtr piece[9], original[9];
+ScenePtr scene;
+
+//조각들 위치 설정
+const int x[3] = { 255,430,605 };
+const int y[3] = { 400,660,920 };
+
+int game_index(ObjectPtr object) {
+	for (int i = 0;i < 9;i++) {
+		if (piece[i] == object)	return i;
+	};
 }
 
-int main()
+//index와 blank를 바꿔준다.
+void game_move(int index)
 {
-	ScenePtr scene;
-	//필요없는 옵션 제거
-	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
-	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
+	ObjectPtr temp = piece[index];
+	piece[index] = piece[blank];
+	piece[index]->locate(scene, x[index % 3], y[2 - index / 3]);
+	piece[blank] = temp;
+	piece[blank]->locate(scene, x[blank % 3], y[2 - blank / 3]);
+	blank = index;
+}
 
+//빈칸옆인지 확인하는 함수
+bool check(int index) {
+	if (blank %3>0 && index == blank - 1) return true;
+	if (blank %3<2 && index == blank + 1) return true;
+	if (index == blank - 3)  return true;
+	if (index == blank + 3)	 return true;
+
+	return false;
+}
+
+//랜덤으로 숫자를 뽑는다.
+int randmove() {
+	int index = rand() % 9;
+	return index;
+}
+
+//셔플해주는 함수
+void game_set() {
+	for (int i = 0;i < 10;i++) {
+		game_move(randmove());
+	}
+}
+
+//게임 종료를 확인
+bool checkend() {
+	for (int i = 0;i < 9;i++) {
+		if (original[i] != piece[i]) return false;
+	}
+	return true;
+}
+
+//게임의 진행을 하는 함수
+void Start() {
 	scene = Scene::create("퍼즐 ", "Images/배경.png");
-
-	
-	//조각들 위치 설정, 이미지 배정
-	const int x[3] = { 255,425,590 };
-	const int y[3] = { 400,660,915 };
-	auto piece1 = Object::create("Images/모나리자1.png", scene,x[1], y[2]);
-	piece1->setScale(0.5f);
-	Piece piece_1(x[1], y[2], piece1,scene);
-	auto piece2 = Object::create("Images/모나리자2.png", scene, x[0], y[2]);
-	piece2->setScale(0.5f);
-	Piece piece_2(x[0], y[2], piece2, scene);
-	auto piece3 = Object::create("Images/모나리자3.png", scene, x[1], y[1]);
-	piece3->setScale(0.5f);
-	Piece piece_3(x[1], y[1], piece3, scene);
-	auto piece4 = Object::create("Images/모나리자4.png", scene, x[0], y[0]);
-	piece4->setScale(0.5f);
-	Piece piece_4(x[0], y[0], piece4, scene);
-	auto piece5 = Object::create("Images/모나리자5.png", scene, x[2], y[2]);
-	piece5->setScale(0.5f);
-	Piece piece_5(x[2], y[2], piece5, scene);
-	auto piece6 = Object::create("Images/모나리자6.png", scene, x[2], y[1]);
-	piece6->setScale(0.5f);
-	Piece piece_6(x[2], y[1], piece6, scene);
-	auto piece7 = Object::create("Images/모나리자7.png", scene, x[0], y[1]);
-	piece7->setScale(0.5f);
-	Piece piece_7(x[0], y[1], piece7, scene);
-	auto piece8 = Object::create("Images/모나리자8.png", scene, x[1], y[0]);
-	piece8->setScale(0.5f);
-	Piece piece_8(x[1], y[0], piece8, scene);
-	auto blank = Object::create("Images/빈칸.png", scene, x[2], y[0]);
-	blank->setScale(0.5f);
-	Piece Blank(x[2], y[0], blank, scene);
-
-	int a, b;
-
-	//마우스로 클릭하면 빈칸으로 이동하도록 설정
-
-	piece1->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_1, &Blank);
-		a = piece_1.x;
-		b = piece_1.y;
-		piece_1.x = Blank.x;
-		piece_1.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	piece2->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_2, &Blank);
-		a = piece_2.x;
-		b = piece_2.y;
-		piece_2.x = Blank.x;
-		piece_2.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	piece3->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_3, &Blank);
-		a = piece_3.x;
-		b = piece_3.y;
-		piece_3.x = Blank.x;
-		piece_3.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	piece4->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_4, &Blank);
-		a = piece_4.x;
-		b = piece_4.y;
-		piece_4.x = Blank.x;
-		piece_4.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	piece5->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_5, &Blank);
-		a = piece_5.x;
-		b = piece_5.y;
-		piece_5.x = Blank.x;
-		piece_5.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	piece6->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_6, &Blank);
-		a = piece_6.x;
-		b = piece_6.y;
-		piece_6.x = Blank.x;
-		piece_6.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	piece7->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_7, &Blank);
-		a = piece_7.x;
-		b = piece_7.y;
-		piece_7.x = Blank.x;
-		piece_7.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	piece8->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		change(&piece_8, &Blank);
-		a = piece_8.x;
-		b = piece_8.y;
-		piece_8.x = Blank.x;
-		piece_8.y = Blank.y;
-		Blank.x = a;
-		Blank.y = b;
-		return true;
-		});
-
-	//게임을 종료하는 버튼
-	auto button = Object::create("Images/성공.png", scene, 0, 400);
-	button->setScale(0.2f);
-	button->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		endGame();
-		return true;
-		});
-
-	//게임방법 설명하는 버튼 생성
-	auto howto = Object::create("Images/게임방법.png", scene, 0, 1000);
-	howto->setScale(0.8f);
-
-	auto explain = Object::create("Images/how.png", scene, 25, 600, false);
-	explain->setScale(1.5f);
-
-	howto->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		explain->show();
-		return true;
-		});
-
-	explain->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
-		explain->hide();
-		return true;
-		});
-
+	char path[30];
 	//60초 타이머 생성
 	auto timer1 = Timer::create(60);
 	showTimer(timer1);
-	timer1->start();
 
 	timer1->setOnTimerCallback([&](TimerPtr timer)->bool {
 		showMessage("실패ㅠㅠ");
@@ -190,7 +81,80 @@ int main()
 		return true;
 		});
 
+	long long t1;
+	//시작하는 버튼 생성
+	auto button = Object::create("Images/시작.png", scene, 0, 400);
+	button->setScale(0.2f);
+	button->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		timer1->start();
+		button->hide();
+		game_set();
+		showMessage("게임 시작!");
+	     t1 = time(NULL);
+		return true;
+		});
 
-	showMessage("제한시간은 1분! 퍼즐을 풀어보세요. ");
+	for (int i = 0;i < 9;i++)
+	{
+		sprintf(path, "Images/모나리자_00%d.JPEG", i + 1);
+		piece[i] = Object::create(path, scene, x[i % 3], y[2 - i / 3]);
+		piece[i]->setScale(0.5f);
+		piece[i]->setOnMouseCallback([&](auto object, int x, int y, auto action)->bool {
+			int num = game_index(object);
+			if (check(num)) {
+				game_move(num);
+				if (checkend()) {
+					timer1->stop();
+					long long t2= time(NULL);
+					int a = t2 - t1;
+					char s1[100];
+					sprintf(s1, "성공!!!!!! %d초 걸렸습니다.", a);
+					showMessage(s1);
+					auto timer2 = Timer::create(1);
+					timer2->start();
+
+					timer2->setOnTimerCallback([&](TimerPtr timer)->bool {
+						endGame();
+						return true;
+						});
+				}
+			}
+			return true;
+			});
+		original[i] = piece[i];
+	}
+	blank = 8;
+	piece[blank]->hide();
+
+	
+
+
+	//게임방법 설명하는 버튼 생성
+	auto howto = Object::create("Images/게임방법.png", scene, 0, 1000);
+	howto->setScale(0.8f);
+
+	auto explain = Object::create("Images/how.png", scene, 150, 450, false);
+
+
+	howto->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		explain->show();
+		return true;
+		});
+
+	explain->setOnMouseCallback([&](ObjectPtr object, int x, int y, MouseAction action)->bool {
+		explain->hide();
+		return true;
+		});
+
 	startGame(scene);
+}
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	//필요없는 옵션 제거
+	setGameOption(GameOption::GAME_OPTION_INVENTORY_BUTTON, false);
+	setGameOption(GameOption::GAME_OPTION_MESSAGE_BOX_BUTTON, false);
+
+	Start();
 }
